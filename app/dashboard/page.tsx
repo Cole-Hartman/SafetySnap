7dbee6149d3df7bc500f1b8139f925b3c115d1e6
cole-hartman <colehartman00@gmail.com>
2025-04-04 19:06:17 -0700

    u

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
 import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button"
 
 export default async function ProtectedPage() {
 	const supabase = await createClient();
